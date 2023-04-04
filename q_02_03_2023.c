b1790a9cc0c7a97ad0e1bf185dc18e775917efd7
Raushan Kumar <103691584+raushanthakur12345@users.noreply.github.com>
2023-04-04 23:30:45 +0530

    Add files via upload

---
+++ q_02_03_2023.c
@@ -0,0 +1,10 @@
+//Write a C program to calculate factorial of an integer number taking input from thecommand line.
+#include<stdio.h>
+int main(int argc, char *argv[]){
+	int i, s;
+	//x = atoi(argv[1]); 
+	//y = atoi(argv[2]);
+	s=s*i;
+	printf("\n Sum = %d",s);
+	return 0;
+}
